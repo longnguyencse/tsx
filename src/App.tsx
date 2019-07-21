b65ad6c893d6bfabc759989261df4a7186cc5544
Long Nguyen <longnq@itvsoft.vn>
2019-07-21 14:12:47 +0700

    https://www.dylanpaulus.com/2018-5-16-using-typescript-with-react-part-one.1/

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React  from 'react';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface PieceProps {
+    color?: "red" | "black";
+}
+interface State {
+    count?: any;
 }
 
+class App extends React.Component<PieceProps, State>{
+
+    constructor(props: PieceProps) {
+        super(props);
+        this.state = {
+                    count: 0
+        };
+    }
+
+    private loginAction = (event: React.MouseEvent<HTMLElement>) => {
+        console.log("llll");
+    }
+
+    render() {
+        return (
+            <button onClick={this.loginAction}>Login</button>
+        )
+    }
+
+}
 export default App;
